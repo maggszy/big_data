622095fc69e69a6b060a1ca8c12ba29415a0f972
Mateusz Machaj <mateo.mach@gmail.com>
2024-06-24 03:21:29 +0200

    Update run-app.sh

--- spark-cluster/run-app.sh
+++ spark-cluster/run-app.sh
@@ -1,1 +1,2 @@
-docker exec -it spark-master /opt/spark/bin/spark-submit --master spark://172.28.0.2:7077 /opt/app/src/app.py
+export $(grep -v '^#' .env | xargs)
+docker exec -it spark-master /opt/spark/bin/spark-submit --master spark://${MASTER_IP}:7077 /opt/app/src/app.py
